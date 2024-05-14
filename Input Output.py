11121bdb2b44fc6ecfcc51a9162f1c9495dd9ab2
Lonaldinho <168597448+Lonaldinho@users.noreply.github.com>
2024-05-15 10:12:10 +1200

    Add files via upload
    Input Output DGT

---
+++ Input Output.py
@@ -0,0 +1,28 @@
+#ask the user for their name
+username = input("what is your name? ")
+
+#ask the user or their favourite number (integer)
+fav_num = in(input("What is your favourite number? "))
+
+#Double, halve and square the user's favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+# Greet the user
+print("Hi (username), your favorite number is (fav_num)")
+
+# Output the results of doubling, halving and 
+# squaring their favourite integer
+print("Double (fav_num) is (double) ,")
+
+# Output the results of doubling, halving and
+# squaring their favourite integer
+print(f"Double (fav_num) is (double).")
+print(f"Half (fav_num) is (halve)")
+print(f"(fav_num) is (halve)")
+print()
+print("have a nice day.")
+
+
+

71890dcca166932c331b489f976615f89349838c
Lonaldinho <168597448+Lonaldinho@users.noreply.github.com>
2024-06-05 10:18:57 +1200

    Update Variables part 1.py

--- Variables part 1.py
+++ Variables part 1.py
@@ -1,11 +1,13 @@
-greting = "hello world"
+# Code to learn about variables...
 
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
 
+# Variables can be a number 
 fav_num = 7
-nun_string = "5"
+num_string = "5"
 
-
+# Constants are uppercase
 PI = 3.14
 
-
-a_01_my_num = 7
+my_num = 7

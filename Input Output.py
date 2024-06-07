8d34589ece2f014304bc4390b3619e1750158e00
Lonaldinho <168597448+Lonaldinho@users.noreply.github.com>
2024-06-07 14:06:05 +1200

    Update Input Output.py

--- Input Output.py
+++ Input Output.py
@@ -1,28 +1,24 @@
 #ask the user for their name
 username = input("what is your name? ")
 
-#ask the user or their favourite number (integer)
-fav_num = in(input("What is your favourite number? "))
+# ask the user for their favourite number (integer)
+fav_num = int(input("What is your favourite number? "))
 
-#Double, halve and square the user's favourite number
+# Double, halve and square the user's favourite number 
 double = fav_num * 2
 halve = fav_num / 2
 square = fav_num * fav_num
 
-# Greet the user
-print("Hi (username), your favorite number is (fav_num)")
+# Greet the user 
+
+print("Hi {username}, your favourite number is {fav_num}")
 
 # Output the results of doubling, halving and 
-# squaring their favourite integer
-print("Double (fav_num) is (double) ,")
-
-# Output the results of doubling, halving and
-# squaring their favourite integer
-print(f"Double (fav_num) is (double).")
-print(f"Half (fav_num) is (halve)")
-print(f"(fav_num) is (halve)")
+#squaring their favourite integer
+print(f"Double {fav_num} is {double}.")
+print(f"half {fav_num} id {halve}")
+print(f"{fav_num} squared is {square}")
 print()
 print("have a nice day.")
-
 
 
